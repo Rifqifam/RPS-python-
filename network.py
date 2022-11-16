47f9da8de2c3eaf5df20e9aa72fe86b87285cc6c
Rifqifam <muhammad.rifqi.fameizy@mail.ugm.ac.id>
2022-11-16 22:19:49 +0700

    finished rps game

--- network.py
+++ network.py
@@ -1,27 +1,28 @@
 import socket
+import pickle
 
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = "192.168.0.141"
-        self.port = 4411
+        self.port = 5555
         self.addr = (self.server, self.port)
-        self.pos = self.connect()
+        self.p = self.connect()
 
-    def getPos(self):
-        return self.pos
+    
+    def getP(self):
+        return self.p
 
     def connect(self):
-
         try:
             self.client.connect(self.addr)
-            return self.client.recv(2048*2).decode()
+            return self.client.recv(2048).decode()
         except:
             pass
 
     def send(self, data):
         try:
             self.client.send(str.encode(data))
-            return self.client.recv(2048).decode()
+            return pickle.loads(self.client.recv(2048*2))
         except socket.error as e:
             print(e)
